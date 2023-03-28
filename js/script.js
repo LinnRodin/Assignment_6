3e04712925f75a998b55b9cf31cdee0049d82171
Linn Rodin <ael.rodin@gmail.com>
2023-03-28 21:46:21 +0200

    updated

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
-// strict mode 
-'use strict';
-
 try {
-    // Find the button with the "data-option" attribute set to "toggle"
-    const toggleButton = document.querySelector('[data-option="toggle"]');
-    
-    // Get the value of the "data-target" attribute on the toggle button, if it exists
-    const target = toggleButton?.getAttribute('data-target');
-
-    // Define a function to handle click events on the toggle button
-    function handleClick() {
-        console.log('test');
-
-        // Find the element with the ID specified in the "data-target" attribute of the toggle button
-        const element = document.querySelector(target);
-
-        // Toggle the "hide" class on the element, if it exists
-        if (element) {
-            element.classList.toggle('hide');
-        } else {
-            // Log an error message to the console if the element can't be found
-            console.error(`Could not find element with target "${target}"`);
+    const toggleBtn = document.querySelector('[data-option="toggle"]')
+    toggleBtn.addEventListener('click', function() {
+        const element = document.querySelector(toggleBtn.getAttribute('data-target'))
+        
+        if (!element.classList.contains('open-menu')) {
+            element.classList.add('open-menu')
+            toggleBtn.classList.add('btn-outline-dark')
+            toggleBtn.classList.add('btn-toggle-white')
         }
-    }
 
-    // Add a click event listener to the toggle button, with the handleClick function as the callback
-    toggleButton?.addEventListener('click', handleClick);
-} catch (err) {
-    // Log an error message to the console if an error occurs
-    console.error(`An error occurred: ${err}`);
-}
+        else {
+            element.classList.remove('open-menu')
+            toggleBtn.classList.remove('btn-outline-dark')
+            toggleBtn.classList.remove('btn-toggle-white')
+        }
+    })    
+} catch {}
